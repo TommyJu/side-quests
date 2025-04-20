b2e455d40384a0a8ddad2bc767a0fd75f3d59280
Tommy Ju <tju1@my.bcit.ca>
2025-04-20 16:02:19 -0700

    START project! :)

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace im_bored.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
