861240f66a08b2beb8e8d5b2daef617b0d077f45
Tommy Ju <tju1@my.bcit.ca>
2025-04-22 19:52:41 -0700

    REMOVE unused user authentication functionality

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -6,6 +6,7 @@ namespace im_bored.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    // public int Points { get; set; } = 0;
     // Relationship: list of activities this user has saved
     public ICollection<Activity> Activities { get; set; } = [];
 }
