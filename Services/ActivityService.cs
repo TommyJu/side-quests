6742f86a1d53974871821db22b277ad34dfde67d
Tommy Ju <tju1@my.bcit.ca>
2025-04-22 18:52:08 -0700

    IMPLEMENT saving activities

--- Services/ActivityService.cs
+++ Services/ActivityService.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Security.Claims;
 using im_bored.Data;
 using im_bored.Models;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace im_bored.Services;
 
-public class ActivityService(ApplicationDbContext _context, AuthenticationStateProvider _authenticationStateProvider)
+public class ActivityService(
+    ApplicationDbContext _context,
+    UserManager<ApplicationUser> _userManager)
 {
     // Select random activity from the database
     public async Task<Activity> GetRandomActivity()
@@ -25,19 +29,81 @@ public class ActivityService(ApplicationDbContext _context, AuthenticationStateP
     ActivityPrice? price = null,
     ActivityDuration? duration = null,
     bool? kidFriendly = null)
-{
-    var query = _context.Activities.AsQueryable();
+    {
+        var query = _context.Activities.AsQueryable();
 
-    if (type.HasValue) query = query.Where(a => a.Type == type.Value);
-    if (participants.HasValue) query = query.Where(a => a.Participants == participants.Value);
-    if (price.HasValue) query = query.Where(a => a.Price == price.Value);
-    if (duration.HasValue) query = query.Where(a => a.ActivityDuration == duration.Value);
-    if (kidFriendly.HasValue) query = query.Where(a => a.kidFriendly == kidFriendly.Value);
+        if (type.HasValue) query = query.Where(a => a.Type == type.Value);
+        if (participants.HasValue) query = query.Where(a => a.Participants == participants.Value);
+        if (price.HasValue) query = query.Where(a => a.Price == price.Value);
+        if (duration.HasValue) query = query.Where(a => a.ActivityDuration == duration.Value);
+        if (kidFriendly.HasValue) query = query.Where(a => a.kidFriendly == kidFriendly.Value);
 
-    var activities = await query.ToListAsync();
-    if (activities.Count == 0) return null;
+        var activities = await query.ToListAsync();
+        if (activities.Count == 0) return null;
 
-    var random = new Random();
-    return activities[random.Next(activities.Count)];
-}
-}
+        var random = new Random();
+        return activities[random.Next(activities.Count)];
+    }
+
+    /*
+            * Save activity to the user's saved activities list
+            * @param principal The ClaimsPrincipal of the user
+            * @param activity The activity to save
+            * @return Task representing the asynchronous operation
+
+    */
+    public async Task SaveActivityAsync(ClaimsPrincipal principal, Activity activity)
+    {
+        var user = await _userManager.GetUserAsync(principal);
+        if (user == null || activity == null) return;
+
+        // Attach tracked entities if necessary
+        _context.Attach(user);
+        _context.Attach(activity);
+
+        if (!user.Activities.Any(a => a.Id == activity.Id))
+        {
+            user.Activities.Add(activity);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    /*
+                * Remove activity from the user's saved activities list
+                * @param principal The ClaimsPrincipal of the user
+                * @param activity The activity to remove
+                * @return Task representing the asynchronous operation
+    */
+    public async Task RemoveActivityAsync(ClaimsPrincipal principal, Activity activity)
+    {
+        var user = await _userManager.GetUserAsync(principal);
+        if (user == null || activity == null) return;
+
+        // Attach tracked entities if necessary
+        _context.Attach(user);
+        _context.Attach(activity);
+
+        if (user.Activities.Any(a => a.Id == activity.Id))
+        {
+            user.Activities.Remove(activity);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    /*
+            * Get the list of saved activities for the user
+            * @param principal The ClaimsPrincipal of the user
+            * @return List of saved activities
+    */
+    public async Task<List<Activity>> GetSavedActivitiesAsync(ClaimsPrincipal principal)
+    {
+        var user = await _userManager.GetUserAsync(principal);
+        if (user == null) return [];
+
+        return await _context.Users
+            .Where(u => u.Id == user.Id)
+            .SelectMany(u => u.Activities)
+            .ToListAsync();
+    }
+
+} // end of class
